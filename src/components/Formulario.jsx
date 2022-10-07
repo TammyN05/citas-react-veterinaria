78ba9a6b8809767f5c558e7da9cef6c2eb5f757a
TammyN05 <tammy.html@gmail.com>
2022-10-07 14:28:14 -0300

    primero

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div className='md:w-1/2 lg:w-2/5'>
+        <h2 className='font-black text-3xl text-center'>
+          Seguimiento pacientes</h2>
+
+          <p>Añade pacientes y {''}
+            <span className='text-indigo-500 font-bold'>Administralos</span>
+          </p>
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div className='md:w-1/2 lg:w-3/5'>
+        
+    </div>
+  )
+}
+
+export default ListadoPacientes
